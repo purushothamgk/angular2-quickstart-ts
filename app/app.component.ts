d739d7de0ab4e427c5b3288fee5e1b4ccc9f8aaf
purushothamgk <purushi.gkp@gmail.com>
2016-06-11 14:17:20 +0530

    Complete Routing and Clean ups

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,20 @@ import { Component }       from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 @Component({
   selector: 'my-app',
- template: `
-  <h1>{{title}}</h1>
-  <a [routerLink]="['Heroes']">Heroes</a>
-  <router-outlet></router-outlet>
-`,
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
@@ -22,7 +29,20 @@ import { HeroesComponent } from './heroes.component';
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
-  }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+ },
+ {
+  path: '/detail/:id',
+  name: 'HeroDetail',
+  component: HeroDetailComponent
+ }
+
+
 ])
 
 export class AppComponent {
