3f150c9c92ef7eb8d575b14a778efda5041ad1a6
purushothamgk <purushi.gkp@gmail.com>
2016-06-13 07:20:17 +0530

    Http routing added - in-memory web api.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -29,4 +29,26 @@ export class HeroesComponent implements OnInit {
   gotoDetail() {
     this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
   }
+
+  addHero() {
+  this.addingHero = true;
+  this.selectedHero = null;
+ }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  delete(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error); // TODO: Display error message
+  }
+
 }
