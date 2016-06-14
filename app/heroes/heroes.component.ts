aba2e6fe9c9e0ba2725c189b9edecb5751f407f4
purushothamgk <purushi.gkp@gmail.com>
2016-06-14 16:24:15 +0530

    Bug fix

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -1,45 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Router } from '@angular/router-deprecated';
-
-import { Hero } from '../hero';
+import { Router }            from '@angular/router-deprecated';
+import { Hero }                from '../hero';
+import { HeroService }         from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
-import { HeroService } from '../hero.service';
-
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes/heroes.component.html',
   styleUrls:  ['app/heroes/heroes.component.css'],
   directives: [HeroDetailComponent]
 })
-
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
+  addingHero = false;
+  error: any;
   constructor(
     private router: Router,
     private heroService: HeroService) { }
   getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService
+        .getHeroes()
+        .then(heroes => this.heroes = heroes)
+        .catch(error => this.error = error); // TODO: Display error message
   }
-  ngOnInit() {
-    this.getHeroes();
-  }
-  onSelect(hero: Hero) { this.selectedHero = hero; }
-  gotoDetail() {
-    this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
-  }
-
   addHero() {
-  this.addingHero = true;
-  this.selectedHero = null;
- }
-
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
   close(savedHero: Hero) {
     this.addingHero = false;
     if (savedHero) { this.getHeroes(); }
   }
-
   delete(hero: Hero, event: any) {
     event.stopPropagation();
     this.heroService
@@ -50,5 +41,14 @@ export class HeroesComponent implements OnInit {
         })
         .catch(error => this.error = error); // TODO: Display error message
   }
-
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.addingHero = false;
+  }
+  gotoDetail() {
+    this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+  }
 }
